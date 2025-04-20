b0dc040bfacadd78601eda8faa8f6e671789e8d2
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-04-20 20:58:35 +0600

    banner section end

--- my-project/src/components/Banner.jsx
+++ my-project/src/components/Banner.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import bannerImage from '../../src/assets/banner-jpg.jpg'
+
 const Banner = () => {
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                    src={bannerImage}
                     className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
                     <h1 className="text-5xl font-bold">Box Office News!</h1>
@@ -13,7 +15,7 @@ const Banner = () => {
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
                     </p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <button className="btn btn-primary">Get Apointment</button>
                 </div>
             </div>
         </div>
