adb8e489647cf443df685cf2d5f90bdd19d6fe8a
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-04-22 18:08:07 +0600

    modal section done almost 70%

--- my-project/src/components/Modal.jsx
+++ my-project/src/components/Modal.jsx
@@ -1,14 +1,67 @@
 import React from 'react';
 
 const Modal = () => {
+
+    const handleForm=(e)=>{
+        e.preventDefault();
+
+        // console.log(e);
+
+
+        const  fName=e.target.fname.value;
+        const  lName=e.target.lname.value;
+    
+
+        console.log(fName,lName)
+    }
+
     return (
 
-        // < button className = "btn" onClick = {()=> document.getElementById('my_modal_5').showModal()}> open modal</button >
 
         <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
             <div className="modal-box">
-                <h3 className="font-bold text-lg">Hello!</h3>
-                <p className="py-4">Press ESC key or click the button below to close</p>
+
+                <form onSubmit={handleForm}>
+                 
+                        <fieldset className="fieldset">
+                            First Name
+                            <input type="text" name='fname' className="input" placeholder="Type here" />
+                        </fieldset>
+                  
+                        <fieldset className="fieldset">
+                            Last Name
+                            <input type="text" name='lname' className="input" placeholder="Type here" />
+                        </fieldset>
+                   
+                        <fieldset className="fieldset">
+                            Email
+                            <input type="text" name='email' className="input" placeholder="Type here" />
+                        </fieldset>
+                   
+                        <fieldset className="fieldset">
+                            Phone Number
+                            <input type="text" name='number'  className="input" placeholder="Type here" />
+                        </fieldset>
+          
+              
+                        <fieldset className="fieldset">
+                            Appointment Data
+                            <input type="date" className="input" placeholder="Type here" />
+                        </fieldset>
+                 
+          
+                        <fieldset className="fieldset">
+                            Address
+                            <input type="text" className="input" placeholder="Type here" />
+                        </fieldset>
+               
+
+                    <div className='text-center mt-6'>
+                    <button  className='btn btn-primary'>Make Appointment</button>
+                    </div>
+
+                </form>
+
                 <div className="modal-action">
                     <form method="dialog">
                         {/* if there is a button in form, it will close the modal */}
