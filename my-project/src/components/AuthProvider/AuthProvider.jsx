50e1070a99bce627150a8e6889d114d34a36045f
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-04-22 21:10:51 +0600

    now context api is working with firebase

--- my-project/src/components/AuthProvider/AuthProvider.jsx
+++ my-project/src/components/AuthProvider/AuthProvider.jsx
@@ -1,21 +1,38 @@
 import React, {  createContext } from 'react';
-// import { createUserWithEmailAndPassword } from 'firebase/auth';
-// import auth from '../../utilities/firebase.init';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import auth from '../../utilities/firebase.init';
 
 
 export const authContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
 
-    // const handleSignup=(email,passwrod)=>{
-    //    return createUserWithEmailAndPassword(auth,email,passwrod);
-    // }
+    const provider = new GoogleAuthProvider();
 
-    const name= 'utsob';
+    const handleSignup=(email,passwrod)=>{
+       return createUserWithEmailAndPassword(auth,email,passwrod);
+    }
+
+    const handleLogin=(email,passwrod)=>{
+        return signInWithEmailAndPassword(auth,email,passwrod);
+    }
+
+    const handleLogout=()=>{
+        signOut(auth);
+    }
+
+    const hadleGoogle=()=>{
+        signInWithPopup(auth, provider)
+    }
+
+
 
     const authInfo={
-        name,
-        // handleSignup
+    
+        handleSignup,
+        handleLogin,
+        handleLogout,
+        hadleGoogle
     }
 
     return (
--- my-project/src/utilities/firebase.init.js
+++ my-project/src/utilities/firebase.init.js
@@ -6,50 +6,17 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+  apiKey: "AIzaSyAeuf1g4Bp2THSOO-rrIVoAJl1k05qoPG4",
+  authDomain: "recap-email-password-login.firebaseapp.com",
+  projectId: "recap-email-password-login",
+  storageBucket: "recap-email-password-login.firebasestorage.app",
+  messagingSenderId: "325733446899",
+  appId: "1:325733446899:web:7f4f6b84418b4defdeb386"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+// Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
 export default auth;
-
-
-
-
-
-
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getAuth } from "firebase/auth";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBvaH1bE0-vOa4oJwpCiPS45JePmckIhCo",
-//   authDomain: "civic-visitor-453109-h2.firebaseapp.com",
-//   projectId: "civic-visitor-453109-h2",
-//   storageBucket: "civic-visitor-453109-h2.firebasestorage.app",
-//   messagingSenderId: "77311518196",
-//   appId: "1:77311518196:web:29e8d3f907661a537985a1",
-//   measurementId: "G-P9D55KZL3F"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// const auth=getAuth(analytics);
-
-// export default auth;
