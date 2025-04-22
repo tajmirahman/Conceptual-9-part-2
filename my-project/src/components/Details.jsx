d36d944fd6d551755fc9e46ea6daf070a238d8f1
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-04-22 17:47:39 +0600

    modal section start here

--- my-project/src/components/Details.jsx
+++ my-project/src/components/Details.jsx
@@ -1,33 +1,41 @@
 
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Modal from './Modal';
 
 
 const Details = () => {
 
-    const { cost, image, description,name } = useLoaderData();
+    const { image, description,treatment } = useLoaderData();
 
 
     return (
         <div
-            className="hero min-h-screen"
+            className="hero min-h-screen w-[80%] mx-auto max-w-[1200px] mt-8"
             style={{
                 backgroundImage:
-                    "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+                    `url(${image})`,
             }}
         >
             <div className="hero-overlay"></div>
             <div className="hero-content text-neutral-content text-center">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">{name}</h1>
-                    <p className="mb-5">
+                    <h1 className="mb-5 text-5xl font-bold">{treatment}</h1>
+                    <p className="mb-5 text-center">
                         {description}
                     </p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <button onClick = {()=> document.getElementById('my_modal_5').showModal()} className="btn btn-primary">Book Appointment</button>
                 </div>
             </div>
+
+
+            <Modal></Modal>
+
         </div>
+
+        
     );
 };
+
 
 export default Details;
