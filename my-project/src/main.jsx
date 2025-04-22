b4cb9308fd7460fd021c42e45ae098c6ac1a6258
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-04-22 18:55:36 +0600

    firebase section start now

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Routes.jsx'
+import AuthProvider from './components/AuthProvider/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}>
+    <AuthProvider>
+      <RouterProvider router={router}>
 
-    </RouterProvider>
+      </RouterProvider>
+    </AuthProvider>
   </StrictMode>,
 )
